b0a50b8fbfdff1456c48386327e5b1f2171d473f
vince koch <vince.koch@gmail.com>
2021-04-09 00:56:12 -0400

    include static files; improve textarea as query editor

---
+++ Sqlzor/wwwroot/js/textarea-editor.js
@@ -0,0 +1,16 @@
+﻿function configureTextEditor(textareaId, buttonId) {
+    var textarea = document.getElementById(textareaId);
+    var button = document.getElementById(buttonId);
+
+    // handle tabs
+    tabOverride.set(textarea);
+    tabOverride.tabSize(4);
+
+    // ignore F5
+    textarea.addEventListener("keydown", function (e) {
+        if (e.key == "F5") {
+            e.preventDefault();
+            button.click();
+        }
+    });
+}
