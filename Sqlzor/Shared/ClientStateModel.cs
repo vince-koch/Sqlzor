2b238554d31c807e47e6c94d7a05af8bab04d6f4
vince koch <vince.koch@gmail.com>
2022-09-23 15:33:45 -0400

    much more functional now

---
+++ Sqlzor/Shared/ClientStateModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using Sqlzor.Api.Models;
+
+namespace Sqlzor.Shared
+{
+    public class ClientStateModel
+    {
+        public Guid SessionId => Session != null ? Session.SessionId : Guid.Empty;
+
+        public SessionModel Session { get; set; }
+
+        public string QueryText { get; set; } = string.Empty;
+
+        public int CursorX { get; set; } = 0;
+
+        public int CursorY { get; set; } = 0;
+
+        public bool IsBusy { get; set; } = false;
+        
+        public TimeSpan? Elapsed { get; set; } = null;
+        
+        public DataTable[] Tables { get; set; } = null;
+
+        public Exception Error { get; set; } = null;
+
+        public ClientStateModel(SessionModel session)
+        {
+            Session = session;
+        }
+    }
+}
